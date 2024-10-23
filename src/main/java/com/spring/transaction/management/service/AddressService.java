2f03cfb83f682ac5582032f2588108ab46c3948c
Dineshchand Geetha Ravichandran <>
2024-10-23 20:45:31 +0800

    amend annotatioon

--- src/main/java/com/spring/transaction/management/service/AddressService.java
+++ src/main/java/com/spring/transaction/management/service/AddressService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@Transactional(rollbackOn = Exception.class)
 public class AddressService {
 
     @Autowired
